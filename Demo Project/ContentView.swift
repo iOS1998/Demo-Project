abb2c121b206662759b1b62fa514477c90f32543
Jagadish Paul <jagadish.paul@aprodence.com>
2024-04-12 11:39:43 +0530

    Initial Commit

---
+++ Demo Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo Project
+//
+//  Created by MSS on 11/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo Project/Demo_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_ProjectApp.swift
+//  Demo Project
+//
+//  Created by MSS on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
